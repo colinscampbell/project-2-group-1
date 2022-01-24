e9470b8fae19edd96381d4f41598cee302102945
hamcha555 <hammycha@hotmail.com>
2022-01-23 20:08:05 -0600

    update files

---
+++ Resources/fundamentals_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "Fundamentals" (
+    "Ticker" VARCHAR(10)   NOT NULL PRIMARY KEY,
+    "company_name" VARCHAR(55)   NOT NULL,
+    "industry" VARCHAR(255)   NOT NULL,
+    "sub_industry" VARCHAR(255)   NOT NULL
+);
+
+
+
+
